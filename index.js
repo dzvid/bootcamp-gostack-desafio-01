4f49d85757ae5d9a3e4bf682bb6c084e8977f717
tukno <oliveiradavid007@gmail.com>
2019-08-20 19:04:47 -0400

    Server created using Express

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Test nodemon");
+const express = require("express");
+
+//Instancio o servidor
+const server = express();
+
+// Configura o express para tratar o corpo (body) da requisição como JSON
+server.use(express.json());
+
+server.get("/teste", (req, res) => {
+  return res.json({ message: "OK" });
+});
+
+// Servidor ouvindo na porta 3000
+server.listen(3000);
